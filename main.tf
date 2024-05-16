d1c408035a053727236a56db4539142787f43723
FmG <g@example.com>
2024-05-16 18:20:21 +0200

    moved backed to s3

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "harkom-web-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "harkom-web-terraform-state-lock"
+  }
+}
+
 module "cloudfront" {
   source = "./modules/"
 
